59657ba98596a08ae75f06c6a4715ba40f31b5a5
walber <walber@DESKTOP-8M9R5DU>
2023-10-02 11:23:46 -0300

    commiting test2 to branchX

---
+++ src/Test2.java
@@ -0,0 +1,16 @@
+
+public class Test2 {
+//196977
+	public static void main(String[] args) {
+		String hungry = "yes!!!";
+		
+		if (hungry == "yes!!!")
+			System.out.println("eat something!!!");
+		else {
+			System.out.println("work");
+
+	}
+
+}
+}
+
